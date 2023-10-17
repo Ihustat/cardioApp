fa4bd0f078de544f548893403f8d3ed10422651c
Ihustat <ger26dan@gmail.com>
2023-10-17 18:14:12 +0300

    refactor code. Add App class

--- script.js
+++ script.js
@@ -7,77 +7,74 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
-
-let map, mapEvent;
-
 class App {
-    constructor() {
 
+    #map;
+    #mapEvent;
+
+    constructor() {
+        this._getPosition();
+
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        
+        inputType.addEventListener('change', this._toggleClimbField);
     }
 
-    _getPosition() {}
+    _getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),
+                 () =>  {
+                alert('Невозможно получить ваше мостоположение');
+            });
+        };
+    }
 
     _loadMap(position) {
         const {latitude, longitude} = position.coords;
 
         const coords = [latitude, longitude]
     
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
     
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
     
-        map.on('click', (event) => {
-    
-            mapEvent = event;
-    
-            form.classList.remove('hidden');
-            inputDistance.focus();
-        });
+        this.#map.on('click', this._showForm.bind(this));
     }
 
-    _showForm() {}
+    _showForm(e) {
+        this.#mapEvent = e;
+    
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
 
-    _toggleClimbField() {}
+    _toggleClimbField() {
+        inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+        inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+    }
 
-    _newWorkout() {}
+    _newWorkout(e) {
+        e.preventDefault();
+        
+        inputDuration.value = inputDistance.value = inputTemp.value = inputClimb.value = '';
+    
+        const {lat, lng} = this.#mapEvent.latlng;
+    
+        L.marker([lat, lng])
+        .addTo(this.#map)
+        .bindPopup(
+            L.popup({
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+            )
+        .setPopupContent('Тренировка')
+        .openPopup();
+    }
 };
 
-function navigatorSucces(position) {
-
-};
-
-function navigatorFailure() {
-    alert('Невозможно получить ваше мостоположение');
-};
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(navigatorSucces, navigatorFailure);
-};
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    inputDuration.value = inputDistance.value = inputTemp.value = inputClimb.value = '';
-
-    const {lat, lng} = mapEvent.latlng;
-
-    L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-        L.popup({
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-        })
-        )
-    .setPopupContent('Тренировка')
-    .openPopup();
-});
-
-inputType.addEventListener('change', () => {
-    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
-    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
 
